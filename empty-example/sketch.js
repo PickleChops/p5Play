1512901a00c9a453317a4334f65babff35e49f36
Boyd Stratton <bstratton@rmn.com>
2015-06-07 17:06:26 +0100

    A little framerate function

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,19 +1,48 @@
-var x = 0;
-var y = 0;
-var px = 0;
-var py = 0;
-var easing = 0.05;
+/**
+ * Play around with P5 - I used to like Processing, it was very accessible, so trying out p5
+ */
 
+
+var frameRater = null;
+
+/**
+ * Go large
+ */
 function setup() {
-  createCanvas(displayWidth, displayHeight,'webgl');
+	createCanvas(displayWidth, displayHeight, 'webgl');
+
+	frameRater = new ShowFrameRate();
 }
 
 function draw() {
- ellipse(mouseX,mouseY,Math.random()*100,Math.random()*100);
+	ellipse(mouseX, mouseY, Math.random() * 100, Math.random() * 100);
+	frameRater.on();
+}
 
-    push();
-    translate(0,0);
-    fll()
-    text(Math.floor(getFrameRate()), width/2, height/2);
-    pop();
+
+/**
+ * Display frame rate in top left of display
+ */
+function ShowFrameRate() {
+
+	this.textHeight = 25;
+	this.fpsString = 'FPS : ';
+
+	this.on = function () {
+		push();
+		translate(0, 0);
+
+		textSize(16);
+		textAlign(RIGHT);
+
+		var textDisplayWidth = textWidth(this.fpsString + '999');
+
+		fill(255);
+		noStroke();
+		rect(0, 0, textDisplayWidth, this.textHeight);
+		fill(0);
+
+		text(this.fpsString + Math.floor(getFrameRate()), textDisplayWidth, this.textHeight);
+		pop();
+	}
 }
